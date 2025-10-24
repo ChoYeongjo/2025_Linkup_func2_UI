40545692cf1f7dc59d08292e5aa5e4a7058cef2e
ChoYeongjo <145633134+ChoYeongjo@users.noreply.github.com>
2025-10-24 13:18:40 +0900

    Update app_infusion.py

--- app_infusion.py
+++ app_infusion.py
@@ -1,7 +1,7 @@
 import streamlit as st
 # only UI 일단 임시
-st.title("💉CRS Risk Scan (Post-Infusion(24h))")
-st.markdown("**CAR-T 세포 치료제** 주입 후, 추적 관찰 중 24시간 내 **중증 CRS** 발생 여부를 예측합니다.")
+st.title("💉CRS Risk Scan (Post-Infusion)")
+st.markdown("**CAR-T 세포 치료제** 주입 후, 환자 추적 관찰 중 **24시간 내 중증 CRS** 발생 여부를 예측합니다.")
 
 cols=st.columns(7)
 cytokines = ["IL-2", "IL-4", "IL-6", "IL-10", "TNFα", "IFNγ", "IL-17A"]
@@ -17,5 +17,5 @@ for i, col in enumerate(cols):
 
 st.write("사용자가입력한 사이토카인 수치:", cytokine_values)
 
-if st.button("🔍현재 수치 기준 24시간 이내 환자의 중증 CRS 발생 가능성"):
-  st.success(f"결과 출력칸-> 현재 수치 기준 24시간 이내 00.00% ")
+if st.button("🔍24시간 이내 환자의 중증 CRS 발생 가능성"):
+  st.success(f"결과 출력칸-> 측정 수치 시점 기준 환자의 24시간 이내 중증 CRS 발생 가능성은 00.00%입니다.")
